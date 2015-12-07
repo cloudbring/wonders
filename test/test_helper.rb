afe5f7b7fe2d8d26110a657a7dc66533b767f30f
Emmanuel Mwangi <emmanuelm@gmail.com>
2015-12-07 09:17:55 -0800

    Adding minitest config to test/test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,25 +1,25 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/test_help"
-require "minitest/rails"
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+require 'minitest/rails'
 
 # Configured with directions from https://semaphoreci.com/blog/2014/10/27/bdd-on-rails-with-minitest-part-1-up-and-running.html
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
-require "minitest/rails/capybara"
+require 'minitest/rails/capybara'
 
 # Uncomment for awesome colorful output
-require "minitest/pride"
+require 'minitest/pride'
 
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
-  ENV,
-  Minitest.backup_filter
+  ENV
 )
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml for
+  # all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
   ActiveRecord::Migration.check_pending!
